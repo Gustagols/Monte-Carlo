5563f73c50ef653a08eaf175f4691e0679ec6a9e
Gustagols <maltag.tavow@gmail.com>
2025-11-19 04:32:56 -0300

    first commit

---
+++ backend/main.hs
@@ -0,0 +1,33 @@
+{- cabal:
+build-depends: base, scotty, random, text
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import Web.Scotty
+import System.Random.Stateful (uniformRM, globalStdGen)
+import Control.Monad (replicateM)
+
+-- Gera ponto aleatório no quadrado (-1,1)
+randomPoint :: IO (Double, Double)
+randomPoint = do
+    x <- uniformRM (-1.0, 1.0) globalStdGen
+    y <- uniformRM (-1.0, 1.0) globalStdGen
+    return (x, y)
+
+-- Função auxiliar para gerar a lista de pontos
+genPoints :: Int -> IO [(Double, Double)]
+genPoints n = replicateM n randomPoint
+
+main :: IO ()
+main = scotty 3001 $ do
+    get "/" $ do
+        file "index.html"
+
+    get "/images/lightMode-icon.png" $ do
+        file "images/lightMode-icon.png"
+
+    get "/points" $ do
+        n <- param "n"
+        points <- liftAndCatchIO (genPoints n)
+        json points
